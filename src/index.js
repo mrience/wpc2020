6bf61b002680c800a43cae61e980af245299d216
EC2 Default User <ec2-user@ip-172-31-27-106.eu-west-1.compute.internal>
2020-03-16 18:13:41 +0000

    initaal for ui component

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import {authCfg} from './env'
+import {CognitoUserPool, CognitoUserAttribute} from 'amazon-cognito-identity-js'
+
+const userPool = new CognitoUserPool({
+    UserPoolId: authCfg.userPoolId,
+    ClientId: authCfg.clientId
+})
+
+const registerRequest = {
+    email: 'jakub.kanclerz+wpc2020@gmail.com',
+    password: '123qwe'
+}
+
+const registerUser(registerData) => {
+    userPool.signUp(
+        registerData.email,
+        registerData.password,
+        [
+            new CognitoUserAttribute({
+                'Name': 'website',
+                'Value': 'jkan.pl'
+            })
+        ],
+        null,
+        (err, result) => {
+            if (err) {
+                console.error(err);
+            }
+            
+            console.log(result);
+        }
+    )
+}
+
+const registerButton = document.querySelector('.registerUser');
+registerButton.addEventListener('click', () => {
+    console.log(`User ${registerRequest.email} is going to be registered`);
+    registerUser(registerRequest)
+})
